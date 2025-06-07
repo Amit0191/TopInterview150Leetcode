25b08b7e6bf39182b5be833321226d1581323202
Amit Srivastava <amitsrivastava0191@gmail.com>
2025-06-07 14:21:57 +0200

    Added List import.

--- arraystring/best_time_to_buy_and_sell_stock_ii.py
+++ arraystring/best_time_to_buy_and_sell_stock_ii.py
@@ -28,6 +28,10 @@ Output: 0
 Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
 '''
 
+
+from typing import List
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         b = 0
